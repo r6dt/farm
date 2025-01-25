2851192290ef5ee210813bb2243d67920f9f0bdd
r6dt <wycswy7h@gmail.com>
2025-01-26 00:59:25 +0700

    Update r6dt.lua

--- r6dt.lua
+++ r6dt.lua
@@ -25,7 +25,7 @@ _G.GWEBHOOK_LINK = "https://discord.com/api/webhooks/1330044667114291303/dDUp-QK
 _G.GMAIL_RECEIVERS = {"SniperMealtime"}  -- an account to receive hatched Huges etc
 _G.GMAIL_ITEMS = {
   ["All Huges"] = {Class = "Pet", Id = "All Huges", Amount = 1},
-  ["Hype Egg"] = {Class = "Lootbox", Id = "Hype Egg", MinAmount = 1},
+  ["Hype Egg 2"] = {Class = "Lootbox", Id = "Hype Egg", MinAmount = 1},
   ["Daycare egg"] = {Class = "Egg", Id = "Huge Machine Egg 4", MinAmount = 1},
   ["Secret pet1"] = {Class = "Pet", Id = "Rainbow Swirl", MinAmount = 1, AllVariants = true},
   ["Secret pet2"] = {Class = "Pet", Id = "Banana", MinAmount = 1, AllVariants = true},
