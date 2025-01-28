6a1424b3daabbd2e669916a5c76ce405fcc4ce73
r6dt <wycswy7h@gmail.com>
2025-01-29 02:05:17 +0700

    Update Del-CoreGui.lua

--- Del-CoreGui.lua
+++ Del-CoreGui.lua
@@ -1,4 +1,4 @@
-game.Players.PlayerAdded:Connect(function(player)
+    game.Players.PlayerAdded:Connect(function(player)
         player.CharacterAdded:Connect(function(character)
             for _, v in pairs(character:GetDescendants()) do
                 if v:IsA("BasePart") or v:IsA("Decal") then
@@ -7,26 +7,21 @@ game.Players.PlayerAdded:Connect(function(player)
             end
         end)
     end)
-
-
     for _, v in pairs(game:GetDescendants()) do
         if v:IsA("Part") or v:IsA("BasePart") then
             v.Transparency = 1
         end
     end
-
     for i,v in pairs(game.Players.LocalPlayer.PlayerGui:GetChildren()) do
         if v:IsA("ScreenGui") then
             v.Enabled = false
         end
     end
-
     for i, v in pairs(game:GetService("StarterGui"):GetChildren()) do
         if v:IsA("ScreenGui") then
             v.Enabled = false
         end
     end
-
     for i, v in pairs(game:GetService("CoreGui"):GetChildren()) do
         if v:IsA("ScreenGui") then
             v.Enabled = false
