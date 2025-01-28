309df9a9ffab997a853ad3064d000b189f74a0d8
r6dt <wycswy7h@gmail.com>
2025-01-29 04:24:11 +0700

    Update r6dt.lua

--- r6dt.lua
+++ r6dt.lua
@@ -1,6 +1,16 @@
 repeat wait(10) until game:IsLoaded()
+
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/r6dt/farm/main/Del-CoreGui.lua"))()
 task.wait(10)
+
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/r6dt/farm/main/farm.lua"))()
-task.wait(10)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/r6dt/farm/main/fps.lua"))()
+
+
+task.wait(120)
+
+-- ตั้งค่า fps cap ทุกๆ 5 วินาที
+while task.wait(5) do
+    setfpscap(5)
+end
