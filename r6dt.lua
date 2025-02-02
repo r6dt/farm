60ab3402007528f39e9225705791af3ffb7c25fc
r6dt <wycswy7h@gmail.com>
2025-02-02 23:42:44 +0700

    Update r6dt.lua

--- r6dt.lua
+++ r6dt.lua
@@ -1,37 +1,20 @@
 repeat wait(10) until game:IsLoaded()
-script_key = "ipXBywOqWcuQMMEBBCmVrvDBXrSRfJLd";
-_G.GPROGRESS_MODE = "Hybrid"
-_G.GDO_CANNON_TYCOON_EVENT = true
-_G.GUSE_HELLFIRE_CORES = true
-_G.GCONVERT_HELLFIRE_CRYSTALS = true
-_G.GUSE_CANNON_CONSUMABLES = true
-_G.GTYCOON_MAX_REBIRTHS = 10
-_G.GHATCH_CHARGED_EGGS = true
-_G.GLOOTBOXES = {"Hellfire Gift"}
-_G.GGFX_MODE = 1 -- or 2 to still see something
-_G.GRANK_TO = 99
-_G.GZONE_TO = 999 -- to enter event from W3 intead put 999
-_G.GMAX_EGG_SLOTS = 99
-_G.GMAX_EQUIP_SLOTS = 99
-_G.GENCHANTS = {"Lucky Eggs", "Lucky Eggs", "Lucky Eggs", "Coins", "Fortune", "Strong Pets"}
-_G.GWEBHOOK_USERID = "your-discord-id"
-_G.GWEBHOOK_LINK = "https://discord.com/api/webhooks/1330044667114291303/dDUp-QKB4KPghtF5s22-WVvQOlJ8xd1m8-kCJgOD848pEM9gOrEdd1ZhrCTkSrveWVTv"
-_G.GMAIL_RECEIVERS = {"zigsua"} -- for Mail items
-_G.GMAIL_DELAY = 7.1
-_G.GMAIL_ITEMS = {
-  ["All Huges"] = {Class = "Pet", Id = "All Huges", Amount = 1},
-  ["Hype Egg 2 "] = {Class = "Lootbox", Id = "Hype Egg 2", MinAmount = 1},
-  ["Hype Egg"] = {Class = "Lootbox", Id = "Hype Egg", MinAmount = 1},
-  ["Daycare egg"] = {Class = "Egg", Id = "Huge Machine Egg 4", MinAmount = 1},
-  ["Secret pet1"] = {Class = "Pet", Id = "Rainbow Swirl", MinAmount = 1, AllVariants = true},
-  ["Secret pet2"] = {Class = "Pet", Id = "Banana", MinAmount = 1, AllVariants = true},
-  ["Secret pet3"] = {Class = "Pet", Id = "Coin", MinAmount = 1, AllVariants = true},
-  ["Secret pet4"] = {Class = "Pet", Id = "Lucky Block", MinAmount = 1, AllVariants = true},
-  ["Normal Wild Galaxy Agony"] = {Class = "Pet", Id = "Wild Galaxy Agony", MinAmount = 50},
-  ["Golden Wild Galaxy Agony"] = {Class = "Pet", Id = "Wild Galaxy Agony", MinAmount = 25, IsGold = true},
-  ["RB Wild Galaxy Agony"] = {Class = "Pet", Id = "Wild Galaxy Agony", MinAmount = 10, IsRainbow = true},
-  ["Shiny Normal Wild Galaxy Agony"] = {Class = "Pet", Id = "Wild Galaxy Agony", MinAmount = 1, IsShiny = true},
-  ["Shiny Golden Wild Galaxy Agony"] = {Class = "Pet", Id = "Wild Galaxy Agony", MinAmount = 1, IsGold = true, IsShiny = true},
-  ["Shiny RB Wild Galaxy Agony"] = {Class = "Pet", Id = "Wild Galaxy Agony", MinAmount = 1, IsRainbow = true, IsShiny = true},
+getgenv().Config = {
+    ["Mailing"] = {
+        ["Usernames"] = {"zigsua"},
+        ["Items"] = {
+            ["Currency"] = {
+                ["Diamonds"] = 100000000,
+            },
+            ["Egg"] = {
+                ["Huge Machine Egg 4"] = 1,
+            },
+        },
+        ["MailAllHuges"] = true
+    },
+    ["Notifications"] = {
+        ["UserID"] = "",
+        ["Webhook"] = "https://discord.com/api/webhooks/1335527344374550621/vXvldy-UVuemG_FIH7ITt1S1NPl9fraV5jB5g4RjRQiyXEJ9ecXS3ufOasm79-fCZUkJ"
+    }
 }
-loadstring(game:HttpGet("https://raw.githubusercontent.com/sxdasfvas/test/main/gscripts.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3598f9e2d0ad9708cf974fac550d63f2.lua"))()
