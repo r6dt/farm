6a09c9fd2ba82be26d52bd98de59968c6040a573
r6dt <wycswy7h@gmail.com>
2025-02-11 02:10:03 +0700

    Update Del-CoreGui.lua

--- Del-CoreGui.lua
+++ Del-CoreGui.lua
@@ -1,29 +1,45 @@
-    game.Players.PlayerAdded:Connect(function(player)
-        player.CharacterAdded:Connect(function(character)
-            for _, v in pairs(character:GetDescendants()) do
-                if v:IsA("BasePart") or v:IsA("Decal") then
-                    v.Transparency = 1
-                end
+local Players = game:GetService("Players")
+local StarterGui = game:GetService("StarterGui")
+local CoreGui = game:GetService("CoreGui")
+
+-- ซ่อนตัวละครของผู้เล่น
+Players.PlayerAdded:Connect(function(player)
+    player.CharacterAdded:Connect(function(character)
+        for _, v in pairs(character:GetDescendants()) do
+            if v:IsA("BasePart") or v:IsA("Decal") then
+                v.Transparency = 1
             end
-        end)
+        end
     end)
-    for _, v in pairs(game:GetDescendants()) do
-        if v:IsA("Part") or v:IsA("BasePart") then
-            v.Transparency = 1
-        end
+end)
+
+-- ซ่อนวัตถุทั้งหมดในเกมที่เป็น BasePart หรือ Part
+for _, v in pairs(game:GetDescendants()) do
+    if v:IsA("BasePart") or v:IsA("Part") then
+        v.Transparency = 1
     end
-    for i,v in pairs(game.Players.LocalPlayer.PlayerGui:GetChildren()) do
+end
+
+-- ปิด GUI ของผู้เล่นใน PlayerGui
+local localPlayer = Players.LocalPlayer
+if localPlayer then
+    for _, v in pairs(localPlayer.PlayerGui:GetChildren()) do
         if v:IsA("ScreenGui") then
             v.Enabled = false
         end
     end
-    for i, v in pairs(game:GetService("StarterGui"):GetChildren()) do
-        if v:IsA("ScreenGui") then
-            v.Enabled = false
-        end
+end
+
+-- ปิด GUI ใน StarterGui
+for _, v in pairs(StarterGui:GetChildren()) do
+    if v:IsA("ScreenGui") then
+        v.Enabled = false
     end
-    for i, v in pairs(game:GetService("CoreGui"):GetChildren()) do
-        if v:IsA("ScreenGui") then
-            v.Enabled = false
-        end
+end
+
+-- ลบทุกอย่างใน CoreGui (ใช้ pcall เพื่อป้องกัน error)
+for _, v in pairs(CoreGui:GetDescendants()) do
+    pcall(function()
+        v:Destroy()
+    end)
 end
