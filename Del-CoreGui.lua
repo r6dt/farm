c6149f48fdd2d74cb8216f670b3ca3d47156535e
r6dt <wycswy7h@gmail.com>
2025-02-13 19:50:42 +0700

    Update Del-CoreGui.lua

--- Del-CoreGui.lua
+++ Del-CoreGui.lua
@@ -1,23 +1,34 @@
-local player = game.Players.LocalPlayer
-local starterGui = game:GetService("StarterGui")
-local runService = game:GetService("RunService")
+game.Players.PlayerAdded:Connect(function(player)
+        player.CharacterAdded:Connect(function(character)
+            for _, v in pairs(character:GetDescendants()) do
+                if v:IsA("BasePart") or v:IsA("Decal") then
+                    v.Transparency = 1
+                end
+            end
+        end)
+    end)
 
--- ปิดการแสดงผล 3D
-runService:Set3dRenderingEnabled(false)
 
--- ซ่อน UI ของผู้เล่น
-for _, v in ipairs(player.PlayerGui:GetChildren()) do
-    if v:IsA("ScreenGui") then
-        v.Enabled = false
+    for _, v in pairs(game:GetDescendants()) do
+        if v:IsA("Part") or v:IsA("BasePart") then
+            v.Transparency = 1
+        end
     end
-end
 
--- ซ่อน UI ที่มาจาก StarterGui
-for _, v in ipairs(starterGui:GetChildren()) do
-    if v:IsA("ScreenGui") then
-        v.Enabled = false
+    for i,v in pairs(game.Players.LocalPlayer.PlayerGui:GetChildren()) do
+        if v:IsA("ScreenGui") then
+            v.Enabled = false
+        end
     end
-end
 
--- ปิด CoreGui เช่น Leaderboard, Chat ฯลฯ
-starterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All, false)
+    for i, v in pairs(game:GetService("StarterGui"):GetChildren()) do
+        if v:IsA("ScreenGui") then
+            v.Enabled = false
+        end
+    end
+
+    for i, v in pairs(game:GetService("CoreGui"):GetChildren()) do
+        if v:IsA("ScreenGui") then
+            v.Enabled = false
+        end
+end
