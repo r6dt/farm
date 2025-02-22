28722f59d5e3b60fbd5da0159b7c33a2c6b64fce
r6dt <wycswy7h@gmail.com>
2025-02-23 04:59:43 +0700

    Update DDC 2.lua

--- DDC 2.lua
+++ DDC 2.lua
@@ -2,17 +2,22 @@ repeat wait(10) until game:IsLoaded()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/r6dt/farm/main/Del-CoreGui.lua"))()
 task.wait(10)
 script_key = "ipXBywOqWcuQMMEBBCmVrvDBXrSRfJLd";
+
+_G.GDO_CARD_WORLD_EVENT = true
+_G.GCARD_PACK_TO_BUY = "Retro Pack"
+_G.GLOOTBOXES = {"Locked Hype Egg"}
+
 _G.GPROGRESS_MODE = "Hybrid"
 _G.GGFX_MODE = 1-- or 2 to still see something
 _G.GZONE_TO = 999 -- to enter event from W3 intead put 999
 _G.GMAX_EGG_SLOTS = 99
 _G.GMAX_EQUIP_SLOTS = 40
-_G.GLOOTBOXES = {"Locked Hype Egg"}
+_G.GMAX_POWER_FOR_PET_MASTERY_VIA_FUSING = "240t"
 _G.GCLEAR_FAVORITE_PETS = true
 _G.GHATCH_SPEED_MS = 0
 _G.GUSE_FLAGS = {"Fortune Flag","Diamonds Flag","Coins Flag"}
 _G.GPOTIONS = {"Coins","Lucky","Treasure Hunter","Walkspeed","Diamonds","Damage"}
-_G.GENCHANTS = {"Lucky Eggs", "Lucky Eggs", "Happy Pets", "Criticals", "Criticals", "Strong Pets", "Strong Pets", "Huge Hunter", "Tap Power"}
+_G.GENCHANTS = {"Coins","Coins", "Strong Pets", "Criticals","Strong Pets","Strong Pets","Coins","Lucky Eggs", "Treasure Hunter","Diamonds","Tap Power"}
 _G.GMAX_POWER_FOR_PET_MASTERY = "225t"
 _G.GWEBHOOK_USERID = "your-discord-id"
 _G.GWEBHOOK_LINK = "https://discord.com/api/webhooks/1330044667114291303/dDUp-QKB4KPghtF5s22-WVvQOlJ8xd1m8-kCJgOD848pEM9gOrEdd1ZhrCTkSrveWVTv"
